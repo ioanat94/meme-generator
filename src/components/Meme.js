c67157f067a2c34ea3c81702071b56163960f52a
ioanat94 <ioanatiplea94@gmail.com>
2022-05-05 10:29:45 +0300

    Added random meme generation

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,25 @@
+import memesData from '../memesData.js';
+import {useState} from 'react';
+
 function Meme() {
+    let [memeImage, setMemeImage] = useState('');
+
+    function generateMeme() {
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        setMemeImage(memesArray[randomNumber].url);
+    }
+
     return (
         <div  className='meme'>
-            <form className='meme--form'>
+            <div className='meme--form'>
                 <div className='meme--text'>
                     <input type='text' className='meme--text-input' placeholder='Shut up' />
                     <input type='text' className='meme--text-input' placeholder='and take my money' />
                 </div>
-                <button type='button' className='meme--button'>Get a new meme image</button>
-            </form>
+                <button type='button' className='meme--button' onClick={generateMeme}>Get a new meme image</button>
+            </div>
+            <img src={memeImage} alt='Randomly generated meme' className='meme--img' />
         </div>
     )
 }
